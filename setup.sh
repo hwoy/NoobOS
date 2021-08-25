bb767558be5fc68dd2eebbfe59d8d44ec11a8faf
Watthanachai Dueanklang <bosskillerz@gmail.com>
2021-08-25 07:47:19 +0700

    new file:   setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ $1 ]
+then
+curl https://www.googleapis.com/drive/v3/files/1tP7MaqUxOzKQ83qmujZ_JaiB5oQ_ZKHL?alt=media&key=AIzaSyADuDrhQMZQKorT2rEQ5Mrv5dt2MI7CXFM | tar -C $1 -xvJf -
+else
+echo "USGAE: $0 installed_directory"
+fi
