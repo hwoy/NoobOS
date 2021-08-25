614d8db3f9ad7947837dbca625676495e497a7ec
Watthanachai Dueanklang <bosskillerz@gmail.com>
2021-08-25 08:12:15 +0700

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 if [ $1 ]
 then
 curl https://www.googleapis.com/drive/v3/files/1tP7MaqUxOzKQ83qmujZ_JaiB5oQ_ZKHL?alt=media&key=AIzaSyADuDrhQMZQKorT2rEQ5Mrv5dt2MI7CXFM | tar -C $1 -xvJf -
+
+elfi [ $NOS]
+then
+curl https://www.googleapis.com/drive/v3/files/1tP7MaqUxOzKQ83qmujZ_JaiB5oQ_ZKHL?alt=media&key=AIzaSyADuDrhQMZQKorT2rEQ5Mrv5dt2MI7CXFM | tar -C $NOS -xvJf -
+
 else
 echo "USGAE: $0 installed_directory"
 fi
