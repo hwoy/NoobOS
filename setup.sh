b1386ca9a0fe26ac12333696b97d3f6a3dd41ed2
Watthanachai Dueanklang <bosskillerz@gmail.com>
2021-08-25 08:14:51 +0700

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,7 @@ then
 curl https://www.googleapis.com/drive/v3/files/1tP7MaqUxOzKQ83qmujZ_JaiB5oQ_ZKHL?alt=media&key=AIzaSyADuDrhQMZQKorT2rEQ5Mrv5dt2MI7CXFM | tar -C $NOS -xvJf -
 
 else
-echo "USGAE: $0 installed_directory"
+echo "USGAE: $0 install_directory"
+echo "USGAE: $0"
+echo "NOS == install directory env variable"
 fi
