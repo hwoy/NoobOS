2cf5367391ad304a3909fedd309b09645fd3a243
Watthanachai Dueanklang <bosskillerz@gmail.com>
2021-08-25 08:17:50 +0700

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ $1 ]
 then
 curl https://www.googleapis.com/drive/v3/files/1tP7MaqUxOzKQ83qmujZ_JaiB5oQ_ZKHL?alt=media&key=AIzaSyADuDrhQMZQKorT2rEQ5Mrv5dt2MI7CXFM | tar -C $1 -xvJf -
 
-elfi [ $NOS ]
+elif [ $NOS ]
 then
 curl https://www.googleapis.com/drive/v3/files/1tP7MaqUxOzKQ83qmujZ_JaiB5oQ_ZKHL?alt=media&key=AIzaSyADuDrhQMZQKorT2rEQ5Mrv5dt2MI7CXFM | tar -C $NOS -xvJf -
 
